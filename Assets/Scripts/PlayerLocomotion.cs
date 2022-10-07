bce7db74e46a2b07aba55b72ca501356cd318cd5
Shuu <56737334+ShuuFleur@users.noreply.github.com>
2022-10-07 11:54:02 +0200

    Animation Commit
    Add animations and finish some tilesets
    Player Controller fully usable

--- Assets/Scripts/PlayerLocomotion.cs
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerLocomotion : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 1f;
+    public ContactFilter2D movementFilter;
+    public float collisionsOffset = 0.05f;
+
+    Vector2 movementInput;
+    Rigidbody2D rb;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    public bool isMoving { get; private set; } = false;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if(movementInput != Vector2.zero)
+        {
+            bool success = TryMove(movementInput);
+
+            if (!success)
+            {
+                success = TryMove(new Vector2(movementInput.x, 0));
+                if (!success)
+                {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+            }
+
+            isMoving = success;
+        } else {
+            isMoving = false;
+        }
+    }
+
+    private bool TryMove(Vector2 direction)
+    {
+        int count = rb.Cast(direction, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionsOffset);
+
+        if (count == 0)
+        {
+            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void OnMove(InputValue mouvementValue)
+    {
+        movementInput = mouvementValue.Get<Vector2>();
     }
 }
